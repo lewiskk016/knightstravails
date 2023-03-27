17f2207de57ec80fa6655ba1d5665b8414e5c025
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-03-27 16:25:45 -0400

    add to knightpath

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -1,14 +1,27 @@
 class KnightPathFinder
 
-
+    attr_reader :position, :considered_position
 
     def initialize(position)
-        position = [0, 0]
-        
+        @position = position 
+        @considered_position = []
     end
 
 
     def find_path(array)
+
+    end
+
+    def self.valid_moves(pos)
+    possible =  
+            [[1,-2],
+            [1,2],
+            [-1,-2],
+            [-1,2],
+            [2,-1],
+            [2,1],
+            [-2,-1],
+            [-2,1]]
 
     end
 
