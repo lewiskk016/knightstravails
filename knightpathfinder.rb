b96d84835fd731965a7372d084fd990c794c0441
lewiskk016 <lewiskk016@gmail.com>
2023-03-27 15:15:23 -0400

    finished phase 0

---
+++ knightpathfinder.rb
@@ -0,0 +1,24 @@
+class KnightPathFinder
+
+
+
+    def initialize(position)
+        position = [0, 0]
+        
+    end
+
+
+    def find_path(array)
+
+    end
+
+
+
+
+
+
+
+
+
+
+end
---
+++ polytreenode.rb
@@ -0,0 +1,16 @@
+class PolyTreeNode
+
+
+    attr_accessor :root_node
+
+    def initialize
+        root_node = [0,0]
+        build_move_tree = root_node
+         
+    end
+
+    # def build_move_tree()
+
+
+
+end
